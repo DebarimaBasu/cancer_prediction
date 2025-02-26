dd853c82f9c109215cb77a1a61dc8f0838ddde0a
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-02-27 03:33:31 +0530

    installation done

--- cosmos/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import PredictForm from "./PredictForm";
-function App() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <PredictForm />
-    </div>
-  );
-}
-export default App;
