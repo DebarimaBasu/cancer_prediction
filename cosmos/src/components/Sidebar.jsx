cf6d073ce6303d3f8c6b359a6a30cce277f748ff
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-03-02 01:49:01 +0530

    installation

--- cosmos/src/components/Sidebar.jsx
+++ cosmos/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 import { sun } from "../assets";
 import { navlinks } from "../constants";
